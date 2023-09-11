e948801210d0771549e9f5a7dede8352e9dc2cea
Mouhammad Aly <lebdqp@gmail.com>
2023-09-11 14:40:52 +0100

    first commit

---
+++ assets/index.js
@@ -0,0 +1,7 @@
+var small_menu= document.querySelector('.toggle_menu')
+var menu= document.querySelector('.menu')
+small_menu.onclick= function(){
+var small_menu= document.querySelector('.toggle_menu')
+small_menu.classList.toggle('.active')
+menu.classList.toggle('.responsive')
+}
